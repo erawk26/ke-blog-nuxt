bf215d2f05365bd9b93579829d2cb3c4001b6ce8
Erik Olsen <erawk26@gmail.com>
2020-02-05 00:40:10 -0500

    home.json => homepage.json

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ export default {
     if (payload) {
       return payload
     } else {
-      const obj = await require('~/assets/content/home.json')
+      const obj = await require('~/assets/content/homepage.json')
       return obj
     }
   }
