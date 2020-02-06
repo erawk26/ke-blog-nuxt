f7f847c2c7a2f76dc06e9c5dbd001a18297bad29
Erik Olsen <erik.olsen@milespartnership.com>
2020-02-06 18:39:50 -0500

    add a slider component

---
+++ components/Slider.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  .slider
+    transition(mode="out-in" :name="animationName")
+      .slide(v-for="(slide,i) in slides" v-if="active==i" :key="i")
+        h3 {{ slide.title }}
+        p(v-html="$md.render(slide.caption)")
+    v-btn.prev(@click="loop(-1)") prev
+    v-btn.next(@click="loop(1)") next
+</template>
+
+<script>
+export default {
+  props: {
+    slides: {
+      type: Array,
+      default: () => []
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    desc: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      active: 0,
+      animationName: 'slide-right'
+    }
+  },
+  methods: {
+    loop(dir) {
+      const next = this.active + dir
+      this.animationName = dir > 0 ? 'slide-left' : 'slide-right'
+      this.active =
+        next < 0
+          ? this.slides.length - 1
+          : next >= this.slides.length
+          ? 0
+          : next
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
