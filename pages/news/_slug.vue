88eefe2038ed7514059798265147fa146f794908
Erik Olsen <erawk26@gmail.com>
2020-02-06 18:59:37 -0500

    rename from post to slug to stay consistent

---
+++ pages/news/_slug.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  article(:class="$route.name")
+    h1.uc {{title}}
+    div(v-html="$md.render(body)")
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) {
+      return payload
+    } else {
+      const obj = await require(`~/assets/content/news/${params.slug}.json`)
+      return obj
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
