dd18d050ed837a8b9522bd1e2824870a24f2f680
Erik Olsen <erik.olsen@milespartnership.com>
2020-02-07 22:12:28 -0500

    add in calendar component

--- pages/scheduling.vue
+++ pages/scheduling.vue
@@ -10,27 +10,50 @@
           v-date-picker(v-model='dates' @input='dateAdded')
         transition(name="accordion-fade")
           v-combobox(outlined deletable-chips v-model="datesArr" v-if="datesArr.length" :items="datesArr" label="Your Times" multiple chips)
+        v-calendar(ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="week")
         v-textarea(outlined v-model='comments' label="body" required)
         v-btn.mr-4.bold(outlined color='success' @click='submitForm')
           | Send
         v-btn.mr-4.bold(outlined color='error' @click='reset')
-          | Clear   
+          | Clear
 
 </template>
 
 <script>
-// import Slider from '~/components/Slider'
+// eslint-disable-next-line no-unused-vars
+import { compareAsc, format } from 'date-fns'
+
 export default {
-  components: {
-    // Slider
-  },
+  components: {},
   data() {
     return {
       loading: true,
       datepickerIsOpen: false,
       dates: '',
       datesArr: [],
-      comments: ''
+      comments: '',
+      today: format(new Date(), 'yyyy-MM-dd'),
+      events: [
+        {
+          name: 'Weekly Meeting',
+          start: '2019-01-07 09:00',
+          end: '2019-01-07 10:00'
+        },
+        {
+          name: "Thomas' Birthday",
+          start: '2019-01-10'
+        },
+        {
+          name: 'Mash Potatoes',
+          start: '2019-01-09 12:30',
+          end: '2019-01-09 15:30'
+        }
+      ]
     }
   },
   async asyncData({ params, payload }) {
@@ -38,11 +61,11 @@ export default {
       return payload
     } else {
       const obj = await require('~/assets/content/page2/scheduling.json')
-      // obj.slideData = await require('~/assets/content/album/nested-fields-gallery.json')
       return obj
     }
   },
   mounted() {
+    // this.$refs.calendar.scrollToTime('08:00')
     setTimeout(() => {
       this.loading = false
     }, 2000)
