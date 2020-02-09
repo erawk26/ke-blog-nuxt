1e7eaf7dcce071f57c7367d9865d1c8ab16bc93a
Erik Olsen <erawk26@gmail.com>
2020-02-09 17:22:43 -0500

    change main container markup

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,7 @@
       component(is="Header")
       v-app-bar-nav-icon.mobile-only
     v-content.main-layout.full-width.row-1.row-span-1.col-full
-      v-container.px-5.max-pg-width
-        nuxt
+      nuxt
     component(is="Footer").row-2.row-span-1.col-full
 </template>
 
