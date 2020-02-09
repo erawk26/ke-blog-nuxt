a5ab75d293a074c6669d50784b29c706f02fa832
Erik Olsen <erawk26@gmail.com>
2020-02-09 00:56:17 -0500

    another mobile fix

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,8 @@
   .header-wrapper.eo-flex.a-stretch.full-width.max-pg-width
     .cell.site-logo.eo-flex.center
       logo
-    v-spacer
-    .cell.eo-flex.a-center.j-end
-      my-menu.nav-main.menu--main.eo-flex.j-start(v-if="!$vuetify.breakpoint.smAndDown" type="flat" :menu="$store.state.menus.main" hide-icon :parentState="isMini")
+    .cell.eo-flex.a-center.j-end.flex-grow.desktop(v-if="!$vuetify.breakpoint.mdAndDown")
+      my-menu.nav-main.menu--main.eo-flex.j-start(type="flat" :menu="$store.state.menus.main" hide-icon :parentState="isMini")
       //v-menu(bottom left)
         template(v-slot:activator='{ on }')
           v-btn(icon v-on='on')
