6354df367577dbc359ffb53872ec0eca8b00b048
Erik Olsen <erawk26@gmail.com>
2020-02-09 17:05:24 -0500

    dot menu: add in a smaller system style options menu

---
+++ components/DotMenu.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    v-menu.rel(bottom left open-on-hover :close-on-content-click="false")
+        template(v-slot:activator='{ on }')
+            v-btn(icon v-on='on')
+                v-icon mdi-dots-vertical
+        v-list
+            v-list-item
+                v-list-item-action
+                    theme-btn
+            v-list-item(to="/scheduling")
+                v-list-item-title.uc Scheduling
+</template>
+
+<script>
+import ThemeBtn from '~/components/ThemeToggle'
+export default {
+  components: {
+    ThemeBtn
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
